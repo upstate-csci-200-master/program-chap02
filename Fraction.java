5aa1b6e4ae21f1c8ad2ca325ca352ffe9fa2602e
Dan Wooster <60037980+dwoosterup@users.noreply.github.com>
2020-05-22 22:52:01 -0400

    Create Fraction.java

---
+++ Fraction.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fraction
+{
+	public static void main(String[] args)
+	{
+    // declare a scanner object for input
+		Scanner scan = new 
+
+    // prompt for numerator and read in
+    
+    // prompt for denomator and read in
+    
+    // computer the answer and store in a variable of type double
+    
+		// ouutput the answer
+    
+  }
+}
